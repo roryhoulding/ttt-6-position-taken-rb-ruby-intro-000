43ab8050f99141d893200562365fdc57d53a6881
Rory H <rohoulding@googlemail.com>
2019-09-06 15:48:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
     false
+  elsif board[position] == "X" || board[position] == "O"
+    true
   end
 end
