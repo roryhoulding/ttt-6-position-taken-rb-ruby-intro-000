bcacf69f53444058bfd0e921bc7b01e6b343bdd9
Rory H <rohoulding@googlemail.com>
2019-09-06 15:47:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, position)
-  if board[position] == " " || board[position] == ""
+  if board[position] == " " || board[position] == "" || board[position] == nil
     false
   end
 end
